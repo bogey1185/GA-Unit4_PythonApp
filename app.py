d86bd5486742ee69f11fa7882386285e10b4c10c
Binod <binod@Binitas-MacBook-Air.local>
2019-02-27 10:31:49 -0700

    added the basics on the app.py page

---
+++ app.py
@@ -0,0 +1,33 @@
+
+## do we need to import resources??
+from flask import Flask, g, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_bcrypt import check_password_hash
+
+import models
+import forms
+
+app = flask(__name__)
+
+app.secret_key = ""
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+
+if __name__ == '__main__':
+    models.initialize()
+
+    app.run(debug = True, port = 8000)
